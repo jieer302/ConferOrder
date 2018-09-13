5a4f28655f4872780172b113c1f21ce985318dba
jieer302 <34927851+jieer302@users.noreply.github.com>
2018-09-13 14:57:26 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
-/**
- * Created by swhy-wl on 2018/5/9.
- */
 
-var webpack = require('webpack');//引入Webpack模块供我们调用，这里只能使用ES5语法，使用ES6语法会报错
+var webpack = require('webpack');
 
-//__dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
 module.exports = {//注意这里是exports不是export
     devtool: 'eval-source-map',//生成Source Maps,这里选择eval-source-map
     entry: ['webpack/hot/dev-server', __dirname + '/src/main.js'],//唯一入口文件,__dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
@@ -40,11 +36,9 @@ module.exports = {//注意这里是exports不是export
     //webpack-dev-server配置
     devServer: {
         contentBase: './build',//默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
-       // colors: true,//在cmd终端中输出彩色日志
-        historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        historyApiFallback: true,
         inline: true,//设置为true，当源文件改变时会自动刷新页面
         port: 8080,//设置默认监听端口，如果省略，默认为"8080"
-       // process: true,//显示合并代码进度
     },
 
    performance: {
